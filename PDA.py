cf8fb125c5c5144a4532e5b1db409cc9e8cb4ed8
Constantin Gabriel <constantin.gab02@gmail.com>
2020-05-05 19:02:08 +0300

    first commit

--- PDA.py
+++ PDA.py
@@ -1,4 +1,12 @@
 f = open('date.in')
-matrix = [['0' for x in range(100)] for y in range(100)]
-for x in f.readline().split():
-    print(x, len(x))
+matrix = [['0' for x in range(1000)] for y in range(1000)]
+ini = 0
+fin = 0
+for x in f.readlines():
+    if len(x.split()) == 2:
+        ini = int(x.split()[0])
+        fin = int(x.split()[1])
+        print(ini, fin)
+    else:
+        matrix[ini][(int(x.split(' ', 1)[0]) if x.split(' ', 1)[0] < 'a' else ord(x.split(' ', 1)[0]))] = (x.split(' ', 1)[1].replace('\n', '')+' '+str(fin))
+        print((x.split(' ', 1)[1].replace('\n', '')+' '+str(fin)))
